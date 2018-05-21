24c4794f132c63c5836dcc46e75104e474493708
RajaVamsi Chidella <rajavamsi.chidella@gmail.com>
2018-05-21 15:40:20 -0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace UsingMethodsExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSub1 = new System.Windows.Forms.Label();
+            this.lblSub2 = new System.Windows.Forms.Label();
+            this.gBoxStdt1 = new System.Windows.Forms.GroupBox();
+            this.lblSub3 = new System.Windows.Forms.Label();
+            this.gBoxStdt2 = new System.Windows.Forms.GroupBox();
+            this.lblSub1S2 = new System.Windows.Forms.Label();
+            this.lblSub2S2 = new System.Windows.Forms.Label();
+            this.lblSub3S2 = new System.Windows.Forms.Label();
+            this.btnStdt1 = new System.Windows.Forms.Button();
+            this.btnStdt2 = new System.Windows.Forms.Button();
+            this.lstBoxOutputS1 = new System.Windows.Forms.ListBox();
+            this.lstBoxOutputS2 = new System.Windows.Forms.ListBox();
+            this.tBSub1S1 = new System.Windows.Forms.TextBox();
+            this.tBSub2S1 = new System.Windows.Forms.TextBox();
+            this.tBSub3S1 = new System.Windows.Forms.TextBox();
+            this.tBoxSub1S2 = new System.Windows.Forms.TextBox();
+            this.tBoxSub2S2 = new System.Windows.Forms.TextBox();
+            this.tBoxSub3S2 = new System.Windows.Forms.TextBox();
+            this.gBoxStdt1.SuspendLayout();
+            this.gBoxStdt2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblSub1
+            // 
+            this.lblSub1.AutoSize = true;
+            this.lblSub1.Location = new System.Drawing.Point(56, 40);
+            this.lblSub1.Name = "lblSub1";
+            this.lblSub1.Size = new System.Drawing.Size(45, 17);
+            this.lblSub1.TabIndex = 0;
+            this.lblSub1.Text = "Sub 1";
+            // 
+            // lblSub2
+            // 
+            this.lblSub2.AutoSize = true;
+            this.lblSub2.Location = new System.Drawing.Point(56, 96);
+            this.lblSub2.Name = "lblSub2";
+            this.lblSub2.Size = new System.Drawing.Size(45, 17);
+            this.lblSub2.TabIndex = 1;
+            this.lblSub2.Text = "Sub 2";
+            // 
+            // gBoxStdt1
+            // 
+            this.gBoxStdt1.Controls.Add(this.tBSub3S1);
+            this.gBoxStdt1.Controls.Add(this.tBSub2S1);
+            this.gBoxStdt1.Controls.Add(this.tBSub1S1);
+            this.gBoxStdt1.Controls.Add(this.lblSub3);
+            this.gBoxStdt1.Controls.Add(this.lblSub1);
+            this.gBoxStdt1.Controls.Add(this.lblSub2);
+            this.gBoxStdt1.Location = new System.Drawing.Point(75, 48);
+            this.gBoxStdt1.Name = "gBoxStdt1";
+            this.gBoxStdt1.Size = new System.Drawing.Size(288, 191);
+            this.gBoxStdt1.TabIndex = 2;
+            this.gBoxStdt1.TabStop = false;
+            this.gBoxStdt1.Text = "Student 1";
+            // 
+            // lblSub3
+            // 
+            this.lblSub3.AutoSize = true;
+            this.lblSub3.Location = new System.Drawing.Point(56, 147);
+            this.lblSub3.Name = "lblSub3";
+            this.lblSub3.Size = new System.Drawing.Size(45, 17);
+            this.lblSub3.TabIndex = 2;
+            this.lblSub3.Text = "Sub 3";
+            // 
+            // gBoxStdt2
+            // 
+            this.gBoxStdt2.Controls.Add(this.tBoxSub3S2);
+            this.gBoxStdt2.Controls.Add(this.tBoxSub2S2);
+            this.gBoxStdt2.Controls.Add(this.tBoxSub1S2);
+            this.gBoxStdt2.Controls.Add(this.lblSub3S2);
+            this.gBoxStdt2.Controls.Add(this.lblSub2S2);
+            this.gBoxStdt2.Controls.Add(this.lblSub1S2);
+            this.gBoxStdt2.Location = new System.Drawing.Point(465, 48);
+            this.gBoxStdt2.Name = "gBoxStdt2";
+            this.gBoxStdt2.Size = new System.Drawing.Size(269, 191);
+            this.gBoxStdt2.TabIndex = 3;
+            this.gBoxStdt2.TabStop = false;
+            this.gBoxStdt2.Text = "Student 2";
+            // 
+            // lblSub1S2
+            // 
+            this.lblSub1S2.AutoSize = true;
+            this.lblSub1S2.Location = new System.Drawing.Point(39, 40);
+            this.lblSub1S2.Name = "lblSub1S2";
+            this.lblSub1S2.Size = new System.Drawing.Size(45, 17);
+            this.lblSub1S2.TabIndex = 1;
+            this.lblSub1S2.Text = "Sub 1";
+            // 
+            // lblSub2S2
+            // 
+            this.lblSub2S2.AutoSize = true;
+            this.lblSub2S2.Location = new System.Drawing.Point(39, 96);
+            this.lblSub2S2.Name = "lblSub2S2";
+            this.lblSub2S2.Size = new System.Drawing.Size(45, 17);
+            this.lblSub2S2.TabIndex = 2;
+            this.lblSub2S2.Text = "Sub 2";
+            // 
+            // lblSub3S2
+            // 
+            this.lblSub3S2.AutoSize = true;
+            this.lblSub3S2.Location = new System.Drawing.Point(39, 147);
+            this.lblSub3S2.Name = "lblSub3S2";
+            this.lblSub3S2.Size = new System.Drawing.Size(45, 17);
+            this.lblSub3S2.TabIndex = 3;
+            this.lblSub3S2.Text = "Sub 3";
+            // 
+            // btnStdt1
+            // 
+            this.btnStdt1.Location = new System.Drawing.Point(161, 285);
+            this.btnStdt1.Name = "btnStdt1";
+            this.btnStdt1.Size = new System.Drawing.Size(153, 56);
+            this.btnStdt1.TabIndex = 3;
+            this.btnStdt1.Text = "Calculate Student 1";
+            this.btnStdt1.UseVisualStyleBackColor = true;
+            this.btnStdt1.Click += new System.EventHandler(this.btnStdt1_Click);
+            // 
+            // btnStdt2
+            // 
+            this.btnStdt2.Location = new System.Drawing.Point(556, 285);
+            this.btnStdt2.Name = "btnStdt2";
+            this.btnStdt2.Size = new System.Drawing.Size(152, 56);
+            this.btnStdt2.TabIndex = 4;
+            this.btnStdt2.Text = "Calculate Student 2";
+            this.btnStdt2.UseVisualStyleBackColor = true;
+            this.btnStdt2.Click += new System.EventHandler(this.btnStdt2_Click);
+            // 
+            // lstBoxOutputS1
+            // 
+            this.lstBoxOutputS1.FormattingEnabled = true;
+            this.lstBoxOutputS1.ItemHeight = 16;
+            this.lstBoxOutputS1.Location = new System.Drawing.Point(102, 368);
+            this.lstBoxOutputS1.Name = "lstBoxOutputS1";
+            this.lstBoxOutputS1.Size = new System.Drawing.Size(261, 212);
+            this.lstBoxOutputS1.TabIndex = 5;
+            // 
+            // lstBoxOutputS2
+            // 
+            this.lstBoxOutputS2.FormattingEnabled = true;
+            this.lstBoxOutputS2.ItemHeight = 16;
+            this.lstBoxOutputS2.Location = new System.Drawing.Point(473, 368);
+            this.lstBoxOutputS2.Name = "lstBoxOutputS2";
+            this.lstBoxOutputS2.Size = new System.Drawing.Size(261, 212);
+            this.lstBoxOutputS2.TabIndex = 6;
+            // 
+            // tBSub1S1
+            // 
+            this.tBSub1S1.Location = new System.Drawing.Point(160, 40);
+            this.tBSub1S1.Name = "tBSub1S1";
+            this.tBSub1S1.Size = new System.Drawing.Size(100, 22);
+            this.tBSub1S1.TabIndex = 3;
+            // 
+            // tBSub2S1
+            // 
+            this.tBSub2S1.Location = new System.Drawing.Point(160, 91);
+            this.tBSub2S1.Name = "tBSub2S1";
+            this.tBSub2S1.Size = new System.Drawing.Size(100, 22);
+            this.tBSub2S1.TabIndex = 4;
+            // 
+            // tBSub3S1
+            // 
+            this.tBSub3S1.Location = new System.Drawing.Point(160, 144);
+            this.tBSub3S1.Name = "tBSub3S1";
+            this.tBSub3S1.Size = new System.Drawing.Size(100, 22);
+            this.tBSub3S1.TabIndex = 5;
+            // 
+            // tBoxSub1S2
+            // 
+            this.tBoxSub1S2.Location = new System.Drawing.Point(134, 40);
+            this.tBoxSub1S2.Name = "tBoxSub1S2";
+            this.tBoxSub1S2.Size = new System.Drawing.Size(100, 22);
+            this.tBoxSub1S2.TabIndex = 4;
+            // 
+            // tBoxSub2S2
+            // 
+            this.tBoxSub2S2.Location = new System.Drawing.Point(134, 91);
+            this.tBoxSub2S2.Name = "tBoxSub2S2";
+            this.tBoxSub2S2.Size = new System.Drawing.Size(100, 22);
+            this.tBoxSub2S2.TabIndex = 5;
+            // 
+            // tBoxSub3S2
+            // 
+            this.tBoxSub3S2.Location = new System.Drawing.Point(134, 144);
+            this.tBoxSub3S2.Name = "tBoxSub3S2";
+            this.tBoxSub3S2.Size = new System.Drawing.Size(100, 22);
+            this.tBoxSub3S2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 657);
+            this.Controls.Add(this.lstBoxOutputS2);
+            this.Controls.Add(this.lstBoxOutputS1);
+            this.Controls.Add(this.btnStdt2);
+            this.Controls.Add(this.btnStdt1);
+            this.Controls.Add(this.gBoxStdt2);
+            this.Controls.Add(this.gBoxStdt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gBoxStdt1.ResumeLayout(false);
+            this.gBoxStdt1.PerformLayout();
+            this.gBoxStdt2.ResumeLayout(false);
+            this.gBoxStdt2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSub1;
+        private System.Windows.Forms.Label lblSub2;
+        private System.Windows.Forms.GroupBox gBoxStdt1;
+        private System.Windows.Forms.TextBox tBSub3S1;
+        private System.Windows.Forms.TextBox tBSub2S1;
+        private System.Windows.Forms.TextBox tBSub1S1;
+        private System.Windows.Forms.Label lblSub3;
+        private System.Windows.Forms.GroupBox gBoxStdt2;
+        private System.Windows.Forms.TextBox tBoxSub3S2;
+        private System.Windows.Forms.TextBox tBoxSub2S2;
+        private System.Windows.Forms.TextBox tBoxSub1S2;
+        private System.Windows.Forms.Label lblSub3S2;
+        private System.Windows.Forms.Label lblSub2S2;
+        private System.Windows.Forms.Label lblSub1S2;
+        private System.Windows.Forms.Button btnStdt1;
+        private System.Windows.Forms.Button btnStdt2;
+        private System.Windows.Forms.ListBox lstBoxOutputS1;
+        private System.Windows.Forms.ListBox lstBoxOutputS2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsingMethodsExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStdt1_Click(object sender, EventArgs e)
+        {
+            int s1sub1, s1sub2, s1sub3;
+           
+            
+           
+            lstBoxOutputS1.Items.Clear();
+            
+            if (int.TryParse(tBSub1S1.Text, out s1sub1) && 
+                int.TryParse(tBSub2S1.Text, out s1sub2) &&
+                int.TryParse(tBSub3S1.Text, out s1sub3))
+            {
+                lstBoxOutputS1.Items.Add("Scored Max\t" + CalcMaxValue(s1sub1,s1sub2,s1sub3));
+                lstBoxOutputS1.Items.Add("Scored Min\t" + CalcMinValue(s1sub1, s1sub2, s1sub3));
+                lstBoxOutputS1.Items.Add("Total\t\t" + CalTotal(s1sub1, s1sub2, s1sub3));
+                lstBoxOutputS1.Items.Add("Average\t\t" + CalAverage(s1sub1, s1sub2, s1sub3));
+                lstBoxOutputS1.Items.Add("Result\t\t" + CalGrade(s1sub1, s1sub2, s1sub3));
+
+
+            }
+            else
+            {
+                MessageBox.Show("Enter Valid Input", "Invalid Input");
+            }
+           
+        }
+
+        int CalcMaxValue(int a1, int a2, int a3)
+        {
+            if ((a1 > a2) && (a1 > a3))
+            {
+                return a1;
+            }
+            else if ((a2 > a3) && (a2 > a3))
+            {
+                return a2;
+            }
+            else
+            {
+                return a3;
+            }
+        }
+        int CalcMinValue(int a1, int a2, int a3)
+        {
+            if ((a1 < a2) && (a1 < a3))
+            {
+                return a1;
+            }
+            else if ((a2 < a3) && (a2 < a3))
+            {
+                return a2;
+            }
+            else
+            {
+                return a3;
+            }
+
+        }
+        int CalTotal(int a1, int a2, int a3)
+        {
+            int total;
+            total = a1 + a2 + a3;
+            return total;
+        }
+        int CalAverage(int a1, int a2, int a3)
+        {
+            int avg;
+            avg = CalTotal(a1, a2, a3) / 3;
+            return avg;
+        }
+        string CalGrade(int a1, int a2, int a3)
+        {
+            int average;
+            average = CalAverage(a1, a2, a3);
+            if (average > 60 && average < 75)
+            {
+                return "GRADE C";
+            }
+            else if (average > 75 && average < 90)
+            {
+                return "GRADE B";
+            }
+            else if (average > 90)
+            {
+                return "GRADE A";
+            }
+            else
+            {
+                return "GRADE D";
+            }
+        }
+
+        private void btnStdt2_Click(object sender, EventArgs e)
+        {
+            int s2sub1, s2sub2, s2sub3;
+            lstBoxOutputS2.Items.Clear();
+            if (int.TryParse(tBoxSub1S2.Text, out s2sub1) &&
+                int.TryParse(tBoxSub2S2.Text, out s2sub2) &&
+                int.TryParse(tBoxSub3S2.Text, out s2sub3))
+            {
+                lstBoxOutputS2.Items.Add("Scored Max\t" + CalcMaxValue(s2sub1, s2sub2, s2sub3));
+                lstBoxOutputS2.Items.Add("Scored Min\t" + CalcMinValue(s2sub1, s2sub2, s2sub3));
+                lstBoxOutputS2.Items.Add("Total\t\t" + CalTotal(s2sub1, s2sub2, s2sub3));
+                lstBoxOutputS2.Items.Add("Average\t\t" + CalAverage(s2sub1, s2sub2, s2sub3));
+                lstBoxOutputS2.Items.Add("Result\t\t" + CalGrade(s2sub1, s2sub2, s2sub3));
+
+
+            }
+            else
+            {
+                MessageBox.Show("Enter Valid Input", "Invalid Input");
+            }
+        }
+    }
+}
